90dd2940af07ebdd6f506588b5f2070763c23d49
ranakatebi <rana_katebi@hotmail.com>
2022-03-08 19:36:16 +0300

    changed

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -8,21 +8,21 @@ export default function FormattedDate(props) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  
+
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   return (
-    <div>
+    <span>
       {day} {hours}:{minutes}
-    </div>
+    </span>
   );
 }
