6fb4c879cc732d3ad350009c19d03a2f3b47e1b7
ranakatebi <rana_katebi@hotmail.com>
2022-03-11 12:18:38 +0300

    changed default city name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Istanbul" />
+        <Weather defaultCity="Tabriz" />
       </div>
     </div>
   );
